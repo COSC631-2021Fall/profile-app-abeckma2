df47bfd557a390d3a7089e4abf821eb5f4b9f660
Allan <abeckma2@emich.edu>
2021-10-05 22:07:24 +0000

    attempt

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -5,7 +5,7 @@ import Commerce from "@chec/commerce.js";
 function App() {
   const commerce=new Commerce('pk_34213fc645bbd971c6fbe34f2189fb62e6a32eb61a00b');
   commerce.products.list().then(response=>console.log(response.data));
-  /*return (
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );*/
+  );
 }
 
-//export default App;
+export default App;
