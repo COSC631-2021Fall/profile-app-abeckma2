ae8dba39cdb1ead182ca5cdc843eb0968f57fbad
Allan <abeckma2@emich.edu>
2021-10-05 21:58:06 +0000

    here

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -4,6 +4,7 @@ import Commerce from "@chec/commerce.js";
 
 function App() {
   const commerce=new Commerce('pk_34213fc645bbd971c6fbe34f2189fb62e6a32eb61a00b');
+  commerce.products.list().then(response=>console.log(response.data));
   return (
     <div className="App">
       <header className="App-header">
