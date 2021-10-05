4aba6c4878d7fb97b957dab798e61b0d2ee92ac4
Allan <abeckma2@emich.edu>
2021-10-05 22:16:15 +0000

    final

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -7,19 +7,7 @@ function App() {
   commerce.products.list().then(response=>console.log(response.data));
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
