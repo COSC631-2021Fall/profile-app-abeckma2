071e01e0f833434c8837ab4192d2f5c16b155070
Allan <abeckma2@emich.edu>
2021-10-10 12:19:30 +0000

    react continue fixing minor adjusttments 7

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -15,6 +15,7 @@ function App() {
   const useStateArray=["white","#eeeeee","powderblue"];
   const colorArray=["black","#202A44","brown"];
   const[themeId,setThemeId]=useState(0);
+  const[colorId,setColorId]=useState(0);
   const switchTheme=()=>{
     setThemeId((id)=>(id+1)%3);
   }
