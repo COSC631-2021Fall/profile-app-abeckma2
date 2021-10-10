8918417bf9d3911d24d948178b9afad5d9500d50
Allan <abeckma2@emich.edu>
2021-10-10 12:55:21 +0000

    react continue

--- profile-app/src/components/module4.js
+++ profile-app/src/components/module4.js
@@ -3,9 +3,9 @@ function Module4()
 {
     return(
     <div>
-        <p>my email is abeckma2@emich.edu</p>
-          <h2>Module4:Git {id}</h2>
-          {id && <p>my email is abeckma2@emich.edu</p>}
+            <p>my email is abeckma2@emich.edu</p>
+            <h2>Module4:Git {id}</h2>
+            {id && <p>my email is abeckma2@emich.edu</p>}
     </div>
     );
 }
@@ -13,9 +13,9 @@ function LastModule()
 {
     return(
     <div>
-        <p>This is done through EMU </p>
-        <h2>Module5:Git {id}</h2>
-        {id && <p>my email is abeckma2@emich.edu</p>}
+            <p>This is done through EMU </p>
+            <h2>Module5:Git {id}</h2>
+            {id && <p>my email is abeckma2@emich.edu</p>}
     </div>
     );
 }
