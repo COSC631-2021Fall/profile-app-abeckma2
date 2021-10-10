753ecb4d6b0b1bf5951faca83041574449f7a105
Allan <abeckma2@emich.edu>
2021-10-10 08:34:12 +0000

    react continue testxiii

--- profile-app/src/components/module1.js
+++ profile-app/src/components/module1.js
@@ -2,10 +2,10 @@ import {useParams} from "react-router";
 function Module1({par})
 {
     let{id}=useParams();
-    return
-      (<div>
+    return(
+    <div>
           <h2>Module1:Git{id}</h2>
-          {id&&<p>Git is a distributed version control system.</p>}
-          </div>);
+          {id && <p>Git is a distributed version control system.</p>}
+    </div>);
 }
 export default Module1;
