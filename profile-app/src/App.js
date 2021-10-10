a7dfa2d9db165a348660b431f7f0e2ac35b850c8
Allan <abeckma2@emich.edu>
2021-10-10 09:06:24 +0000

    react continue testxviii

--- profile-app/src/App.js
+++ profile-app/src/App.js
@@ -44,7 +44,7 @@ function App() {
         <BrowserRouter>
           <Route path={["/module1", "/module1/:id"]}><Module1 /></Route>
           <Route path={["/module2","/module2/:id"]}><Module2 /></Route>
-          <Route path={["/module3","/module3/:id"]}><Module2 /></Route>
+          <Route path={["/module3","/module3/:id"]}><Module3 /></Route>
         </BrowserRouter>
         <div><button onClick={switchTheme}>button</button></div>
       </main>
