a01d0cc4fd7a280484d580fc1c4eb74ee03e61ad
Allan <abeckma2@emich.edu>
2021-10-10 11:12:45 +0000

    react continue fixing minor adjusttments 2

--- profile-app/src/components/module4.js
+++ profile-app/src/components/module4.js
@@ -1,11 +1,9 @@
 import {useParams} from "react-router";
-function Module4({par})
+function Module4()
 {
-    let{id}=useParams();
     return(
     <div>
-          <h2>Module4:Git {id}</h2>
-          {id && <p>my email is abeckma2@emich.edu</p>}
+        <p>my email is abeckma2@emich.edu</p>
     </div>
     );
 }
