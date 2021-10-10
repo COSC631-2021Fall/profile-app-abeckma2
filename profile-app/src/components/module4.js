c386845559b5c0e9109da9b9f121f4e8c2902cd3
Allan <abeckma2@emich.edu>
2021-10-10 12:59:11 +0000

    react continue

--- profile-app/src/components/module4.js
+++ profile-app/src/components/module4.js
@@ -4,7 +4,6 @@ function Module4()
     return(
     <div>
             <p>my email is abeckma2@emich.edu</p>
-            <h2>Module4:Git </h2>
     </div>
     );
 }
@@ -13,7 +12,6 @@ function LastModule()
     return(
     <div>
             <p>This is done through EMU</p>
-            <h2>Module5:Git </h2>
     </div>
     );
 }
